21bb9e2c96218cb16cc94ca7393d63a0f67335fb
Cristóbal Sepúlveda <crisep95@gmail.com>
2022-04-14 17:08:23 -0400

    fix: peli fechas command

--- apps/cinehoyts/bot.py
+++ apps/cinehoyts/bot.py
@@ -6,10 +6,14 @@ from discord.ext import commands
 from apps.cinehoyts.services import (
     get_info_cinemas,
     get_info_cities,
-    get_movie_showing_by_cinema,
-    get_movie_showing_by_date,
+    get_showing_by_cinema,
+    get_showing_by_date,
     get_showings,
+    get_showings_by_zone,
+    get_cinema_showings,
+    get_cinema_showings_by_date,
 )
+from apps.cinehoyts.constants import CINEMAS_ZONES
 
 
 def main():
@@ -26,22 +30,33 @@ def main():
         await ctx.send("pong")
 
     @client.command()
-    async def cine_fecha(ctx, cinema: str, date: str, movie: str):
-        cinema_showings = get_showings(cinema, date, movie)
-        await ctx.send(cinema_showings)
-
-    @client.command()
-    async def peli_fecha(ctx, date: str, movie: str):
-        cinema_showings = get_movie_showing_by_date(date, movie)
+    async def horarios(ctx, movie: str, date: str, cinema: str = None):
+        cinema_is_zone = cinema in CINEMAS_ZONES
+        if cinema and not cinema_is_zone:
+            cinema_showings = get_showings(movie, date, cinema)
+        elif cinema and cinema_is_zone:
+            cinema_showings = get_showings_by_zone(movie, date, cinema)
+        else:
+            cinema_showings = get_showing_by_date(movie, date)
         for cinema_showing_part in cinema_showings.split("$SEPARATOR$"):
-            if cinema_showing_part:
+            if cinema_showing_part and cinema_showing_part not in ('\n', '\n\n'):
                 await ctx.send(cinema_showing_part)
 
     @client.command()
-    async def fechas(ctx, cinema: str, movie: str):
-        cinema_showings = get_movie_showing_by_cinema(cinema, movie)
+    async def horarios_cine(ctx, movie: str, cinema: str, format: str = None):
+        cinema_showings = get_showing_by_cinema(movie, cinema, format)
         for cinema_showing_part in cinema_showings.split("$SEPARATOR$"):
-            if cinema_showing_part:
+            if cinema_showing_part and cinema_showing_part not in ('\n', '\n\n'):
+                await ctx.send(cinema_showing_part)
+
+    @client.command()
+    async def cine(ctx, cinema: str, date: str = None):
+        if date:
+            cinema_showings = get_cinema_showings_by_date(cinema, date)
+        else:
+            cinema_showings = get_cinema_showings(cinema)
+        for cinema_showing_part in cinema_showings.split("$SEPARATOR$"):
+            if cinema_showing_part and cinema_showing_part not in ('\n', '\n\n'):
                 await ctx.send(cinema_showing_part)
 
     @client.command()
