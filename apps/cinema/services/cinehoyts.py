5fd51042507b0d8a43bebe1c83f4b0b8a43ae0da
Cristóbal Sepúlveda <crisep95@gmail.com>
2022-05-25 19:56:44 -0400

    fix: datetimes (#8)

--- apps/cinema/services/cinehoyts.py
+++ apps/cinema/services/cinehoyts.py
@@ -64,7 +64,7 @@ def _get_cinema_by_cinema_key(
 
 def _get_showtimes_by_date(cinemas: Dict[str, Any], date_name: str) -> Dict[str, Any]:
     for date in cinemas["Dates"]:
-        if date["ShowtimeDate"] == date_name:
+        if date["ShowtimeDate"].replace(" ", "-") == date_name:
             return date
     return {}
 
