c8c84d1f422b0a5f87c73995fa8ca88f75ff97b9
Cristóbal Sepúlveda <crisep95@gmail.com>
2022-05-28 20:46:30 -0400

    fix: command settings

--- cinema_showings_bot/settings.py
+++ cinema_showings_bot/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+COMMAND = str(os.environ.get("COMMAND", "$c."))
