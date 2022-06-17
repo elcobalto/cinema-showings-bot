96c6e7821b1128b62c893fbe577312f88c2dc402
Cristóbal Sepúlveda <crisep95@gmail.com>
2022-06-16 21:57:55 -0400

    migrations: first models

---
+++ apps/movie/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+from apps.cinema.models import Cinema
+
+
+class Showing(models.Model):
+    movie_title = models.CharField(max_length=255)
+    cinema = models.ForeignKey(
+        Cinema,
+        on_delete=models.CASCADE,
+        related_name="showings",
+    )
+    date = models.CharField(max_length=255)
+    datetime = models.CharField(max_length=255)
+    format = models.CharField(max_length=255)
+    imdb_id = models.CharField(max_length=255)
